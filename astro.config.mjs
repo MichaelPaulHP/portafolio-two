e13dcb145ad43aa8219e790350277159391c94f5
Michael Paul <michael_h_p_@hotmail.com>
2024-01-14 22:53:42 -0500

    fix: set output: 'static'

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
-  output: 'server',
+  output: 'static',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
